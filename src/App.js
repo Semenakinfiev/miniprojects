aa202fc9fa410b3dabf2668737a2e72aedad2e70
semen <semenakinfiev@mail.ru>
2022-11-24 23:47:58 +0500

    create users project

--- src/App.js
+++ src/App.js
@@ -1,51 +1,61 @@
+import React, { useEffect } from 'react';
 import './index.scss';
-import React from 'react';
-import Game from './Game';
-import Result from './Result';
+import { Success } from './components/Success';
+import { Users } from './components/Users';
 
-const questions = [
-  {
-    title: 'React - это ... ?',
-    variants: ['библиотека', 'фреймворк', 'приложение'],
-    correct: 0,
-  },
-  {
-    title: 'Компонент - это ... ',
-    variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
-    correct: 1,
-  },
-  {
-    title: 'Что такое JSX?',
-    variants: [
-      'Это простой HTML',
-      'Это функция',
-      'Это тот же HTML, но с возможностью выполнять JS-код',
-    ],
-    correct: 2,
-  },
-];
-
-
-
-
+// Тут список пользователей: https://reqres.in/api/users
 
 function App() {
 
-  const [step, setStep] = React.useState(0);
-  const [correct, setCorrect] = React.useState(0);
-  const question = questions[step];
+  const [users, setUsers] = React.useState([]);
+  const [invites, setInvites] = React.useState([]);
+  const [isLoading, setLoading] = React.useState(true);
+  const [searchValue, setSearchValue] = React.useState('');
+  const [success, setSuccess] = React.useState(false);
 
-  const changeStep = (index) => {
-   setStep(step + 1);
+  React.useEffect(() => {
+    fetch('https://reqres.in/api/users')
+    .then((res) => res.json())
+    .then((json) => {
+      setUsers(json.data);
+    })
+    .catch((err) => {
+      console.warn(err);
+      alert('Ошибка при получении пользователей!');
+    })
+    .finally(() => setLoading(false));
+  }, [])
 
-   if(question.correct == index) {
-    setCorrect(correct + 1);
-   }
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value);
   }
+
+  const onClickInvite = (id) => {
+    if(invites.includes(id)) {
+      setInvites(prev => prev.filter(_id => _id !== id));
+    } else {
+      setInvites(prev => [...prev, id]);
+    }
+  }
+
+  const onClickSendInvites = () => {
+    setSuccess(true);
+  }
+
 
   return (
     <div className="App">
-      {step !== questions.length ? <Game step={step} question={question} changeStep={changeStep} questions={questions}/> : <Result correct={correct} questions={questions}/> }
+      {success ? (<Success count={invites.length}/>) : (
+        <Users
+        items={users}
+        isLoading={isLoading}
+        searchValue={searchValue}
+        onChangeSearchValue={onChangeSearchValue}
+        invites={invites}
+        onClickInvite={onClickInvite}
+        onClickSendInvites={onClickSendInvites}
+        />
+      )}
     </div>
   );
 }
