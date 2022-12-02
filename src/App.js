408a0370741827f579889f0620135cf8986bd939
semen <semenakinfiev@mail.ru>
2022-12-02 22:39:46 +0500

    create photo collection

--- src/App.js
+++ src/App.js
@@ -1,57 +1,82 @@
 import React from 'react';
-import { Block } from './Block';
 import './index.scss';
+import { Collection } from './Collection';
+
+const cats = [
+  { name: 'Все' },
+  { name: 'Море' },
+  { name: 'Горы' },
+  { name: 'Архитектура' },
+  { name: 'Города' },
+];
 
 function App() {
-  const [valute, setValute] = React.useState(1);
-  const [fromCurrency, setFromCurrency] = React.useState('RUB');
-  const [toCurrency, setToCurrency] = React.useState('USD');
-  const [fromPrice, setFromPrice] = React.useState(0);
-  const [toPrice, setToPrice] = React.useState(0);
-
+  const [collections, setCollections] = React.useState([]);
+  const [searchValue, setSearchValue] = React.useState('');
+  const [categoryID, setCategoryID] = React.useState(0);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [page, setPage] = React.useState(1);
 
   React.useEffect(() => {
-    fetch('https://www.cbr-xml-daily.ru/daily_json.js')
-    .then(res => res.json())
-    .then((json) => {
-      json["Valute"]["RUB"] = {};
-      json["Valute"]["RUB"]["Value"] = 1;
-      setValute(json.Valute);
-    })
-    .catch((err) => {
-      console.warn(err);
-      alert('Не удалось получить данные!');
-    })
-  }, [])
+    setIsLoading(true);
 
-  const onChangeFromPrice = (value) => {
+    const category = categoryID ? `category=${categoryID}` : '';
 
-    const result = (valute[fromCurrency]["Value"]/valute[toCurrency]["Value"] * value).toFixed(3);
-    setToPrice(result);
-    setFromPrice(value);
-  }
-
-  const onChangeToPrice = (value) => {
-
-    const result = valute[toCurrency]["Value"]/valute[fromCurrency]["Value"] * value;
-    setFromPrice(result);
-    setToPrice(value);
-  }
-
-  const onChangeFromCurrency = (cur) => {
-    setFromCurrency(cur);
-    onChangeFromPrice(fromPrice);
-  }
-
-  const onChangeToCurrency = (cur) => {
-    setToCurrency(cur);
-    onChangeToPrice(toPrice);
-  }
+    fetch(
+      `https://63878d8fd9b24b1be3f4047c.mockapi.io/collections?page=${page}&limit=3&${category}`,
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        setCollections(json);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert('Ошибка при получении данных!');
+      })
+      .finally(() => setIsLoading(false));
+  }, [categoryID, page]);
 
   return (
     <div className="App">
-      <Block value={fromPrice} currency={fromCurrency} onChangeCurrency={onChangeFromCurrency} onChangeValue={onChangeFromPrice}/>
-      <Block value={toPrice} currency={toCurrency} onChangeCurrency={onChangeToCurrency} onChangeValue={onChangeToPrice}/>
+      <h1>Моя коллекция фотографий</h1>
+      <div className="top">
+        <ul className="tags">
+          {cats.map((obj, index) => (
+            <li
+              className={categoryID == index ? 'active' : ''}
+              onClick={() => setCategoryID(index)}
+              key={index}>
+              {obj.name}
+            </li>
+          ))}
+        </ul>
+        <input
+          value={searchValue}
+          onChange={(event) => {
+            setSearchValue(event.target.value);
+          }}
+          className="search-input"
+          placeholder="Поиск по названию"
+        />
+      </div>
+      <div className="content">
+        {isLoading ? (
+          <h2>Идёт загрузка...</h2>
+        ) : (
+          collections
+            .filter((obj) => {
+              return obj.name.toLowerCase().includes(searchValue.toLowerCase());
+            })
+            .map((obj, index) => <Collection key={index} name={obj.name} images={obj.photos} />)
+        )}
+      </div>
+      <ul className="pagination">
+        {[...Array(5)].map((_, i) => (
+          <li onClick={() => setPage(i + 1)} className={page == i + 1 ? 'active' : ''} key={i}>
+            {i + 1}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
